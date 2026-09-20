c70a84e35065478092d9dc7043ec4390aeb08e88
Andrii Traitaka <traitacka@gmail.com>
2026-09-20 12:13:05 +0300

    lb 10

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -16,7 +16,7 @@ const products = [
     {
         id: 2,
         title: "Смартфон Apple iPhone 15 Pro 256GB",
-        price: 99999,
+        price: 89999,
         category: "smartphones",
         image: "assets/img/item_pics/item2.png",
     },
@@ -37,7 +37,7 @@ const products = [
     {
         id: 5,
         title: "Мерч SayGex Shop X DEGAN",
-        price: 39999,
+        price: 200,
         category: "exclusives",
         image: "assets/img/item_pics/item5.png",
     }
@@ -57,5 +57,37 @@ const htmlString = products
     `;
     }).join("");
 
-// Вставляємо згенерований текст на сторінку
 container.innerHTML = htmlString;
+
+let cart = [];
+document.querySelector('.products-grid').addEventListener('click', (event) => {
+    if (event.target.classList.contains('btn')) {
+        const productId = Number(event.target.dataset.id);
+        const selectedProduct = products.find((p) => p.id === productId);
+        addToCart(selectedProduct);
+    }
+});
+function addToCart(product) {
+    const existingItem = cart.find((item) => item.productId === product.id);
+
+    if (existingItem) {
+        existingItem.quantity += 1;
+    } else {
+        cart.push({productId: product.id, productTitle: product.title, productPrice: product.price, quantity: 1});
+    }
+    updateUI();
+}
+function updateUI() {
+    const cartCounter = document.querySelector('.cart-counter');
+    const cartItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+
+    if (cartCounter) {
+        cartCounter.textContent = cartItems;
+    }
+
+    console.log("Кошик: ", cart);
+    console.log("Загальна сума: ", calculateTotal(), "грн");
+}
+function calculateTotal() {
+    return cart.reduce((total, item) => total + item.productPrice * item.quantity, 0);
+}
